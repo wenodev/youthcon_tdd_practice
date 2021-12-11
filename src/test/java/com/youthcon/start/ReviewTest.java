7e98400c2c333892f1f579471492aa25f276d363
wenodev <logicalstark1105@gmail.com>
2021-12-12 01:17:58 +0900

    red: Rewview 생성 실패하는 코드

---
+++ src/test/java/com/youthcon/start/ReviewTest.java
@@ -0,0 +1,12 @@
+package com.youthcon.start;
+
+import org.junit.jupiter.api.Test;
+
+public class ReviewTest {
+
+    @Test
+    void make_review(){
+        Review review = ReviewTest();
+    }
+
+}
