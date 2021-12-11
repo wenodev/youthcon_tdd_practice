a86333ea9408711b7fa9137a855113b9c04aebcc
wenodev <logicalstark1105@gmail.com>
2021-12-12 01:33:04 +0900

    refactor: Review 변수 추가

--- src/main/java/com/youthcon/start/Review.java
+++ src/main/java/com/youthcon/start/Review.java
@@ -1,10 +1,14 @@
 package com.youthcon.start;
 
 public class Review {
+
+    private String content;
+    private String phoneNumber;
+
     public Review(String content, String phoneNumber) {
     }
 
     public String getContent() {
-        return "별로에요";
+        return this.content;
     }
 }
