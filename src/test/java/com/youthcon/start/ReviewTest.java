0bd20617dd581229de3372703f64a8987db5e0c4
wenodev <logicalstark1105@gmail.com>
2021-12-12 01:41:55 +0900

    red: 100글자 미만일때 실패하는 코드

--- src/test/java/com/youthcon/start/ReviewTest.java
+++ src/test/java/com/youthcon/start/ReviewTest.java
@@ -3,6 +3,7 @@ package com.youthcon.start;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ReviewTest {
 
@@ -14,6 +15,16 @@ public class ReviewTest {
         Review review = new Review(content, phoneNumber);
 
         assertThat(review.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void 후기가_100글자_미만이면_예외가_발생한다(){
+        String content = "별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요";
+        String phoneNumber = "010-1234-1234";
+
+        assertThatThrownBy(() ->
+                new Review(content, phoneNumber))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
