4660dcc781e532f2782fe2b2ef840d402a4ec2cb
wenodev <logicalstark1105@gmail.com>
2021-12-12 01:31:10 +0900

    red: review content 테스트 실패

--- src/test/java/com/youthcon/start/ReviewTest.java
+++ src/test/java/com/youthcon/start/ReviewTest.java
@@ -8,9 +8,9 @@ public class ReviewTest {
 
     @Test
     void create_review(){
-        Review review = new Review("너무 재미있어요.", "010-1234-1234");
+        Review review = new Review("별로에요", "010-1234-1234");
 
-        assertThat(review.getContent()).isEqualTo("너무 재미있어요");
+        assertThat(review.getContent()).isEqualTo("별로에요");
     }
 
 }
