79eaa8f543f193d024b4934909e623215ec21a55
wenodev <logicalstark1105@gmail.com>
2021-12-12 01:46:18 +0900

    tip: aaa, gwt 설명

--- src/test/java/com/youthcon/start/ReviewTest.java
+++ src/test/java/com/youthcon/start/ReviewTest.java
@@ -9,21 +9,28 @@ public class ReviewTest {
 
     @Test
     void 후기가_100글자_이상이면_생성된다(){
-        String content = "별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요";
+        // 준비
+        String content = "별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요";
         String phoneNumber = "010-1234-1234";
 
+        // 실행
         Review review = new Review(content, phoneNumber);
 
+        // 검증
         assertThat(review.getContent()).isEqualTo(content);
     }
 
     @Test
     void 후기가_100글자_미만이면_예외가_발생한다(){
+        // 준비
         String content = "별로";
         String phoneNumber = "010-1234-1234";
 
         assertThatThrownBy(() ->
+                // 실행
                 new Review(content, phoneNumber))
+
+                // 검증
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
