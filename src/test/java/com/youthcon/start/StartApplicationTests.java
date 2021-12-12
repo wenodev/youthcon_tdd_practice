0bd3b4bd77a8e954129b41eddf955987b6b045b6
wenodev <logicalstark1105@gmail.com>
2021-12-12 15:30:17 +0900

    re-setup

--- src/test/java/com/youthcon/start/StartApplicationTests.java
+++ src/test/java/com/youthcon/start/StartApplicationTests.java
@@ -1,13 +1,11 @@
 package com.youthcon.start;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,15 +13,5 @@ class StartApplicationTests {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
-
-	@Test
-	void hello(){
-		// 실행
-		ResponseEntity<String> responseEntity = restTemplate.getForEntity("/hello", String.class);
-
-		// 검증
-		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(responseEntity.getBody()).contains("youth");
-	}
 
 }
