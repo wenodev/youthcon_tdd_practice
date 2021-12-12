0818c3496b9648d225b89e92767806076c1f4321
wenodev <logicalstark1105@gmail.com>
2021-12-12 09:26:15 +0900

    red: service에서 repository 테스트 실패

--- src/main/java/com/youthcon/start/ReviewService.java
+++ src/main/java/com/youthcon/start/ReviewService.java
@@ -1,4 +1,7 @@
 package com.youthcon.start;
 
 public class ReviewService {
+    public Review create(String content, String phoneNumber) {
+        return new Review(content, phoneNumber);
+    }
 }
--- src/test/java/com/youthcon/start/ReviewServiceTest.java
+++ src/test/java/com/youthcon/start/ReviewServiceTest.java
@@ -2,18 +2,37 @@ package com.youthcon.start;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class ReviewServiceTest {
+
+    ReviewService reviewService = new ReviewService(reviewRepository);
+
     @Test
-    void 후기_생성(){
+    void 후기_생성_100글자_이상_후기_리턴(){
         // 준비
-        ReviewService reviewService = new ReviewService();
         String content = "1별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요별로에요";
         String phoneNumber = "010-1234-1234";
 
         // 실행
-        reviewService.create(content, phoneNumber);
+        Review review = reviewService.create(content, phoneNumber);
 
         // 검증
-        assertThat(reviewService.create()).isEqualTo();
+        assertThat(review.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void 후기_생성_100글자_이하_예외_발생(){
+        // 준비
+        String content = "별로";
+        String phoneNumber = "010-1234-1234";
+
+        assertThatThrownBy(()->
+                // 실행
+                 reviewService.create(content, phoneNumber))
+
+                // 검증
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
