05feead05adf686d48d6e1960899ae61a4c19ecb
wenodev <logicalstark1105@gmail.com>
2021-12-12 14:04:02 +0900

    통합테스트 샘플

--- src/test/java/com/youthcon/start/StartApplicationTests.java
+++ src/test/java/com/youthcon/start/StartApplicationTests.java
@@ -1,13 +1,29 @@
 package com.youthcon.start;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StartApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void hello(){
+		// 실행
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity("/hello", String.class);
+
+		// 검증
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(responseEntity.getBody()).contains("youth");
 	}
 
 }
