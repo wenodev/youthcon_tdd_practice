fbde4264a539448b7b4611ba9cdbf069a8557d04
wenodev <logicalstark1105@gmail.com>
2021-12-13 16:53:29 +0900

    chore: 불필요한 코드 제거

--- src/test/java/com/youthcon/start/ReviewServiceTest.java
+++ src/test/java/com/youthcon/start/ReviewServiceTest.java
@@ -1,9 +1,6 @@
 package com.youthcon.start;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
