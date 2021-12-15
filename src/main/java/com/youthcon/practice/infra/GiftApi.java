3070046b18789cc6252c2298c2a1f475d7d4511e
wenodev <logicalstark1105@gmail.com>
2021-12-15 14:14:05 +0900

    chore: delete unuded code

--- src/main/java/com/youthcon/practice/infra/GiftApi.java
+++ src/main/java/com/youthcon/practice/infra/GiftApi.java
@@ -1,6 +1,5 @@
 package com.youthcon.practice.infra;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
--- src/test/java/com/youthcon/practice/StartApplicationTests.java
+++ src/test/java/com/youthcon/practice/StartApplicationTests.java
@@ -3,7 +3,6 @@ package com.youthcon.practice;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
